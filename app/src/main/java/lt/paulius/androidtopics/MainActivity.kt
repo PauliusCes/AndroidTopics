3fd009464818039aca73454a79815925ef7b2606
pcesnulevicius <cesnuleviciusp@gmial.com>
2023-05-09 13:41:49 +0300

    Add class ActivityLifecycles

--- app/src/main/java/lt/paulius/androidtopics/MainActivity.kt
+++ app/src/main/java/lt/paulius/androidtopics/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import timber.log.Timber
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ActivityLifecycles() {
 
     lateinit var clickButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,55 +16,12 @@ class MainActivity : AppCompatActivity() {
         clickButton = findViewById(R.id.btnClick)
 
         setUpOnClickListener()
-        timber("onCreate")
 
     }
-
-    override fun onStart() {
-        super.onStart()
-        timber("onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        timber("onResume")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        timber("onStop")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        timber("onPause")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        timber("onDestroy")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        timber("onRestart")
-    }
-
 
     fun setUpOnClickListener() {
         clickButton.setOnClickListener {
             startActivity(Intent(this, SecondActivity::class.java))
         }
-    }
-    fun timber(text: String) {
-
-        Timber.i(
-            """
-                *******************
-                * ${this.localClassName}
-                * $text
-                *******************
-            """.trimIndent()
-        )
     }
 }
--- app/src/main/java/lt/paulius/androidtopics/SecondActivity.kt
+++ app/src/main/java/lt/paulius/androidtopics/SecondActivity.kt
@@ -1,58 +1,12 @@
 package lt.paulius.androidtopics
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import timber.log.Timber
 
-class SecondActivity : AppCompatActivity() {
+class SecondActivity : ActivityLifecycles() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-    }
-
-    override fun onStart() {
-        super.onStart()
-        timber("onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        timber("onResume")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        timber("onStop")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        timber("onPause")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        timber("onDestroy")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        timber("onRestart")
-    }
-
-
-
-    fun timber(text: String) {
-
-        Timber.i(
-            """
-                *******************
-                * ${this.localClassName}
-                * $text
-                *******************
-            """.trimIndent()
-        )
+        timber("onCreate")
     }
 }
