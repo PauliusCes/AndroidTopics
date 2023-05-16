95f0cdf3234037121a0b8873928457a7bc463263
pcesnulevicius <cesnuleviciusp@gmial.com>
2023-05-16 13:55:47 +0300

    Print SaveInstanceState and RestoreInstanceState results with lifecycles

--- app/src/main/java/lt/paulius/androidtopics/MainActivity.kt
+++ app/src/main/java/lt/paulius/androidtopics/MainActivity.kt
@@ -1,10 +1,12 @@
 package lt.paulius.androidtopics
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
+import androidx.activity.result.contract.ActivityResultContracts
 import timber.log.Timber
 
 class MainActivity : ActivityLifecycles() {
@@ -19,23 +21,15 @@ class MainActivity : ActivityLifecycles() {
         clickButton = findViewById(R.id.btnClick)
         itemListView = findViewById(R.id.itemListView)
 
-        setUpOnClickListener()
 
         val items = mutableListOf<Item>()
         generateListOfItems(items)
 
-        adapter = CustomAdapter(this)
-        adapter.add(items)
-        adapter.add(Item(101, "text01", "text02"))
-        adapter.add(
-            Item(102, "text01", "text02"),
-            Item(103, "text01", "text02"),
-            Item(104, "text01", "text02"),
-            Item(105, "text01", "text02")
-        )
+        setUpListView()
+        updateAdapter(items)
 
-        itemListView.adapter = adapter
-
+        setClickOpenItemDetails()
+        setUpOnClickListener()
 
     }
 
@@ -52,9 +46,62 @@ class MainActivity : ActivityLifecycles() {
 
     }
 
+    private fun setUpListView() {
+        adapter = CustomAdapter(this)
+        itemListView.adapter = adapter
+    }
+
+    private fun updateAdapter(items: MutableList<Item>) {
+        adapter.add(items)
+        adapter.add(Item(101, "text01", "text02"))
+        adapter.add(
+            Item(102, "text01", "text02"),
+            Item(103, "text01", "text02"),
+            Item(104, "text01", "text02"),
+            Item(105, "text01", "text02")
+        )
+    }
+
     private fun setUpOnClickListener() {
         clickButton.setOnClickListener {
-            startActivity(Intent(this, SecondActivity::class.java))
+//            startActivity(Intent(this, SecondActivity::class.java))
+            startActivityForResult.launch(Intent(this, SecondActivity::class.java))
         }
+    }
+
+    private fun setClickOpenItemDetails() {
+        itemListView.setOnItemClickListener { adapterView, view, position, l ->
+            val item: Item = adapterView.getItemAtPosition(position) as Item
+
+            val itemIntent = Intent(this, SecondActivity::class.java)
+            itemIntent.putExtra(MAIN_ACTIVITY_ITEM_ID, item.id)
+            itemIntent.putExtra(MAIN_ACTIVITY_ITEM_TEXT01, item.text01)
+            itemIntent.putExtra(MAIN_ACTIVITY_ITEM_TEXT02, item.text02)
+            startActivity(itemIntent)
+        }
+    }
+
+    private val startActivityForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            when (result.resultCode) {
+                Activity.RESULT_OK -> {
+                    val item = Item(
+                        id = result.data
+                            ?.getIntExtra(SecondActivity.SECOND_ACTIVITY_ITEM_ID, 0) ?: 0,
+                        text01 = result.data
+                            ?.getStringExtra(SecondActivity.SECOND_ACTIVITY_ITEM_TEXT01) ?: "",
+                        text02 = result.data
+                            ?.getStringExtra(SecondActivity.SECOND_ACTIVITY_ITEM_TEXT02) ?: "",
+                    )
+
+                    adapter.add(item)
+                }
+            }
+        }
+
+    companion object {
+        const val MAIN_ACTIVITY_ITEM_ID = "lt.paulius.androidtopics_item_id"
+        const val MAIN_ACTIVITY_ITEM_TEXT01 = "lt.paulius.androidtopics_item_text01"
+        const val MAIN_ACTIVITY_ITEM_TEXT02 = "lt.paulius.androidtopics_item_text02"
     }
 }
--- app/src/main/java/lt/paulius/androidtopics/SecondActivity.kt
+++ app/src/main/java/lt/paulius/androidtopics/SecondActivity.kt
@@ -1,12 +1,89 @@
 package lt.paulius.androidtopics
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.EditText
 
 class SecondActivity : ActivityLifecycles() {
+
+    lateinit var idEditText: EditText
+    lateinit var text01EditText: EditText
+    lateinit var text02EditText: EditText
+    lateinit var closeButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        timber("onCreate")
+        idEditText = findViewById(R.id.idEditText)
+        text01EditText = findViewById(R.id.text01EditText)
+        text02EditText = findViewById(R.id.text02EditText)
+        closeButton = findViewById(R.id.closeButton)
+
+//        if (savedInstanceState != null) {
+//            with(savedInstanceState) {
+//                idEditText.setText(getString(SECOND_ACTIVITY_ITEM_ID))
+//                text01EditText.setText(getString(SECOND_ACTIVITY_ITEM_TEXT01))
+//                text02EditText.setText(getString(SECOND_ACTIVITY_ITEM_TEXT02))
+//            }
+//        } else {
+//            getIntentExtra()
+//
+//        }
+
+        getIntentExtra()
+        setClickListenerOfCloseButton()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.run {
+            putString(SECOND_ACTIVITY_ITEM_ID, idEditText.text.toString())
+            putString(SECOND_ACTIVITY_ITEM_TEXT01, text01EditText.text.toString())
+            putString(SECOND_ACTIVITY_ITEM_TEXT02, text02EditText.text.toString())
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        with(savedInstanceState) {
+            idEditText.setText(this.getString(SECOND_ACTIVITY_ITEM_ID))
+            text01EditText.setText(this.getString(SECOND_ACTIVITY_ITEM_TEXT01))
+            text02EditText.setText(this.getString(SECOND_ACTIVITY_ITEM_TEXT02))
+        }
+    }
+
+    private fun getIntentExtra() {
+        idEditText.setText(
+            intent.getIntExtra(MainActivity.MAIN_ACTIVITY_ITEM_ID, -1).toString()
+        )
+        text01EditText.setText(
+            intent.getStringExtra(MainActivity.MAIN_ACTIVITY_ITEM_TEXT01)
+        )
+        text02EditText.setText(
+            intent.getStringExtra(MainActivity.MAIN_ACTIVITY_ITEM_TEXT02)
+        )
+    }
+
+    private fun setClickListenerOfCloseButton() {
+        closeButton.setOnClickListener {
+            val finishIntent = Intent()
+
+            finishIntent.putExtra(SECOND_ACTIVITY_ITEM_ID, (idEditText.text.toString()).toInt())
+            finishIntent.putExtra(SECOND_ACTIVITY_ITEM_TEXT01, text01EditText.text.toString())
+            finishIntent.putExtra(SECOND_ACTIVITY_ITEM_TEXT02, text02EditText.text.toString())
+
+            setResult(RESULT_OK, finishIntent)
+            finish()
+        }
+
+    }
+
+    companion object {
+        const val SECOND_ACTIVITY_ITEM_ID = "lt.paulius.androidtopics.secondactivity_item_id"
+        const val SECOND_ACTIVITY_ITEM_TEXT01 = "lt.paulius.androidtopics.secondactivity_text01"
+        const val SECOND_ACTIVITY_ITEM_TEXT02 = "lt.paulius.androidtopics.secondactivity_text02"
     }
 }
